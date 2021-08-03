63d931e99e6d6d0834361ecd54328f5ced519467
도성곤 <sg.do@unionplace.co.kr>
2021-08-03 14:03:53 +0900

    Mockito 테스트 search() 테스트 완료

--- src/main/java/com/example/mocktest/service/TodoService.java
+++ src/main/java/com/example/mocktest/service/TodoService.java
@@ -4,7 +4,11 @@ import com.example.mocktest.model.TodoEntity;
 import com.example.mocktest.model.TodoRequest;
 import com.example.mocktest.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,10 +22,20 @@ public class TodoService {
         todoEntity.setTitle(request.getTitle());
         todoEntity.setOrder(request.getOrder());
         todoEntity.setCompleted(request.getCompleted());
-        return this.todoRepository.save(todoEntity);
+        return todoRepository.save(todoEntity);
     }
+
     //    2	todo  리스트 목록 중 특정 아이템을 조회
+    public TodoEntity searchById(Long id) {
+        return todoRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
     //    3	todo 리스트 전체 목록을 조회
+    public List<TodoEntity> searchAll() {
+        return todoRepository.findAll();
+    }
+
     //    4	todo 리스트 목록 중 특정 아이템을 수정
     //    5	todo 리스트 목록 중 특정 아이템을 삭제
     //    6	todo 리스트 전체 목록을 삭제
--- src/test/java/com/example/mocktest/service/TodoServiceTest.java
+++ src/test/java/com/example/mocktest/service/TodoServiceTest.java
@@ -9,9 +9,15 @@ import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,5 +39,34 @@ class TodoServiceTest {
 
         TodoEntity todoEntity = this.todoService.add(todoRequest);
         assertEquals(todoRequest.getTitle(), todoEntity.getTitle());
+    }
+
+    @Test
+    void searchById() {
+        TodoEntity entity = new TodoEntity();
+        entity.setId(123L);
+        entity.setTitle("test_title");
+        entity.setOrder(0L);
+        entity.setCompleted(false);
+
+        Optional<TodoEntity> optional = Optional.of(entity);
+
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(optional);
+
+        TodoEntity actual = this.todoService.searchById(123L);
+        TodoEntity expected = optional.get();
+
+        assertEquals(expected.getId(), actual.getId());
+    }
+
+    @Test
+    void searchByIdFaild() {
+        given(this.todoRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        assertThrows(ResponseStatusException.class, () -> {
+            this.todoService.searchById(123L);
+        });
     }
 }
